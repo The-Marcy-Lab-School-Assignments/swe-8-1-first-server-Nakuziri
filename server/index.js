d67a1f5d36e9b917d0aab661080ec8c599e6134e
Xavier Campos <xcampos@brooklynsteamcenter.org>
2025-06-09 12:37:38 -0400

    updated assignment

--- server/index.js
+++ server/index.js
@@ -1,34 +1,50 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require('path');
 
-const servePicture = (req,res) =>{
+const filepath = path.join(__dirname, '../App/dist');
+const serveStatic = express.static(filepath);
+
+const servePicture = (req, res) => {
     res.send({src: "https://static-cdn.jtvnw.net/jtv_user_pictures/meowntain-profile_banner-71b7a6d0d943dc9e-480.jpeg"});
 };
 
-const serveJoke = (req,res) => {
-    res.send({setup: "what do you call a pile of kittens?", puncline: 'a meowtain'})
-}
+const serveJoke = (req, res) => {
+   res.send({setup: "what do you call a pile of kittens?", punchline: "a meowntain"}) 
+};
 
-
-const serveDie = (req,res) => {
+const serveRoll = (req, res) => {
     const {quantity} = req.query;
-    //const quantity = req.query.quantity
-    const length = Number(quantity) > 0? Number(quantity): 1;
+    const length = Number(quantity) > 0? Number(quantity) : 1;
     const rolls = [];
-    for(let i =0; i<length; i++){
-        const roll = Math.ceil(Math.random()*6);
+    for (let i = 0; i < length; i++) {
+        const roll = Math.ceil(Math.random() * 6);
         rolls.push(roll);
-    }
+    };
     res.send(rolls);
-}
+};
 
 
-app.get("/api/picture",servePicture);
-app.get("/api/joke", serveJoke);
-app.get('/api/rollDie', serveDie);
+const logRoutes = (req, res, next) => {
+    const time = (new Date()).toLocaleString();
+    req.time = time;
+    console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+    next();
+  };
+
+
+
+app.use(logRoutes);
+app.use(serveStatic);
+
+
+app.get('/api/picture', servePicture);
+app.get('/api/joke', serveJoke);
+app.get('/api/RollDie', serveRoll);
+
 
 
 const port = 8080;
 app.listen(port, () => {
-    console.log(`listening on http://localhost:${port}`);
-})
+    console.log(`listening at this port: http://localhost:${port}`)
+});
